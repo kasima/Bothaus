5242094e6da26fdf979d306814653bd629aa2c95
Kasima Tharnpipitchai <kasima@gmail.com>
2023-03-09 10:27:31 +0700

    Initial Commit

---
+++ Bothaus/BothausApp.swift
@@ -0,0 +1,17 @@
+//
+//  BothausApp.swift
+//  Bothaus
+//
+//  Created by kasima on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BothausApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
